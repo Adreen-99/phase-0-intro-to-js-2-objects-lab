f66169fa0b88a972588d586381fb925a8ac34382
+ <+githinjiadreen27@gmail.com>
2025-06-16 13:01:19 +0300

    Object lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: 'John Doe', 
+    streetAdress: '4569 Main Street',
+    emailAdress: 'johndoe@gmail.com'  
+};
+
+//Non-destructive 
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+//Destructive
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key, value){
+    employee[key] = value;
+    return employee;
+}
+
+//Delete 
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+//Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
